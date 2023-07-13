4a8a51a1b7ab7d4d498fd0c0380e9c7c0cf2b2ef
SivaniStudies <sivanistudies@gmail.com>
2023-07-13 20:54:52 +0530

    done

--- validate-subsquence/validate-subsequence.java
+++ validate-subsquence/validate-subsequence.java
@@ -1,14 +1,14 @@
-import java.util.*;
-class Program {
-  public static boolean isValidSubsequence(List<Integer> array, List<Integer> sequence) {
-    int arrIdx = 0;
-    int seqIdx = 0;
-    while (arrIdx < array.size() && seqIdx < sequence.size()) {
-      if (array.get(arrIdx).equals(sequence.get(seqIdx))) {
-        seqIdx++;
+  import java.util.*;
+  class Program {
+    public static boolean isValidSubsequence(List<Integer> array, List<Integer> sequence) {
+      int arrIdx = 0;
+      int seqIdx = 0;
+      while (arrIdx < array.size() && seqIdx < sequence.size()) {
+        if (array.get(arrIdx).equals(sequence.get(seqIdx))) {
+          seqIdx++;
+        }
+        arrIdx++;
       }
-      arrIdx++;
+      return seqIdx == sequence.size();
     }
-    return seqIdx == sequence.size();
   }
-}
